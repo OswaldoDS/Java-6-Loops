119f71a0c2a1fb11437505d79a4622b0b65dbaa1
Oswaldo <santoswaldos@gmail.com>
2026-04-07 13:31:00 -0600

    Reto 3, ajustando la validación para dar pistas al usuario

--- src/org/osantos/ejercicios/reto3/EjercicioReto3.java
+++ src/org/osantos/ejercicios/reto3/EjercicioReto3.java
@@ -14,13 +14,21 @@ public class EjercicioReto3 {
 
         byte numeroUser;
 
-        do{
+        do {
             System.out.println("Adivina el número del [1-10]");
             System.out.print("Ingresa un número: ");
             numeroUser = scanner.nextByte();
-        }while (random != numeroUser); //Se ejecuta mientras random sea diferente de numeroUser, es decir, mientras sea true
 
-        System.out.println("\nAdivinaste el número: " + random);
+            //Evaluando los números random y numeroUser
+            if (random == numeroUser){
+                System.out.println("\nAdivinaste el número: " + random);
+            } else if (numeroUser > random) {
+                System.out.println("\nEl número que ingreso es mayor");
+            }else {
+                System.out.println("\nEl número que ingreso es menor");
+            }
+
+        } while (random != numeroUser); //Se ejecuta mientras random sea diferente de numeroUser, es decir, mientras sea true
 
     }
 }
