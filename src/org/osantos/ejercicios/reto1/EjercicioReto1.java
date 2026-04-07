478397b341574ecd8ba740508c691f22b87aa4f5
Oswaldo <santoswaldos@gmail.com>
2026-04-07 12:50:35 -0600

    Reto 1, implementando la clase Random para usar un rango de números específicos de 1 a N (incluyente)

--- src/org/osantos/ejercicios/reto1/EjercicioReto1.java
+++ src/org/osantos/ejercicios/reto1/EjercicioReto1.java
@@ -1,4 +1,21 @@
 package org.osantos.ejercicios.reto1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class EjercicioReto1 {
+    public static void main(String[] args) {
+        //Actividad: Programa generador de número ganador al azar
+        Scanner scanner = new Scanner(System.in);
+
+        //Número total de participantes
+        System.out.print("Ingresa el número total de participantes: ");
+        int participantes = scanner.nextInt();
+
+        //Número ganador usando la Clase Random en vez de la clase Math
+        Random randomObj = new Random();
+        //Se define el rango de 1 a N de forma incluyente
+        int random = 1 + randomObj.nextInt(participantes);
+        System.out.println("El número ganador es: " + random);
+    }
 }
