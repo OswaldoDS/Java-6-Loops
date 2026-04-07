df29030e94148275264ed434c0617ecf1e3d7b85
Oswaldo <santoswaldos@gmail.com>
2026-04-07 13:46:23 -0600

    Reto 4, calculadora de primas de seguro

--- src/org/osantos/ejercicios/reto4/EjercicioReto4.java
+++ src/org/osantos/ejercicios/reto4/EjercicioReto4.java
@@ -1,4 +1,26 @@
 package org.osantos.ejercicios.reto4;
 
+import java.util.Scanner;
+
 public class EjercicioReto4 {
+    public static void main(String[] args) {
+        //Actividad: Desarrollar sistema para calcular las primas mensuales
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa tu edad: ");
+        byte edad = scanner.nextByte();
+
+        System.out.print("Ingresa tus ingresos mensuales: ");
+        double ingresos = scanner.nextDouble();
+
+        if (edad >= 70) {
+            ingresos += 500.0;
+        } else if (edad >= 50 && edad <= 69) {
+            ingresos += 400.0;
+        } else if (edad >= 30 && edad <= 49) {
+            ingresos += 250.0;
+        } else if (edad <= 29) {
+            ingresos += 100.0;
+        }
+        System.out.println("Prima mensual: " + ingresos);
+    }
 }
