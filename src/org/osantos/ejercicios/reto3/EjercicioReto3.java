fb66476b8d5bcee55b1a6c5c1dbf385723639b9f
Oswaldo <santoswaldos@gmail.com>
2026-04-07 13:19:02 -0600

    Reto 3, el usuario saldra del bucle hasta que adivine el número del 1 al 10

--- src/org/osantos/ejercicios/reto3/EjercicioReto3.java
+++ src/org/osantos/ejercicios/reto3/EjercicioReto3.java
@@ -1,4 +1,26 @@
 package org.osantos.ejercicios.reto3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class EjercicioReto3 {
+    public static void main(String[] args) {
+        //Actividad: El usuario debe adivinar el número
+        Scanner scanner = new Scanner(System.in);
+
+        //Se define el rango de 1 a 10 de forma incluyente
+        Random randomObj = new Random();
+        int random = 1 + randomObj.nextInt(10);
+
+        byte numeroUser;
+
+        do{
+            System.out.println("Adivina el número del [1-10]");
+            System.out.print("Ingresa un número: ");
+            numeroUser = scanner.nextByte();
+        }while (random != numeroUser); //Se ejecuta mientras random sea diferente de numeroUser, es decir, mientras sea true
+
+        System.out.println("\nAdivinaste el número: " + random);
+
+    }
 }
