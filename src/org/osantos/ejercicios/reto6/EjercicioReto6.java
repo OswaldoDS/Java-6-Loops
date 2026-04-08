2067167dcc5f266f4eebe42b0fb9769a58f2eb2c
Oswaldo <santoswaldos@gmail.com>
2026-04-07 20:36:42 -0600

    Reto 6, secuencia fibonacci con FOR

--- src/org/osantos/ejercicios/reto6/EjercicioReto6.java
+++ src/org/osantos/ejercicios/reto6/EjercicioReto6.java
@@ -1,4 +1,24 @@
 package org.osantos.ejercicios.reto6;
 
+import java.util.Scanner;
+
 public class EjercicioReto6 {
+    public static void main(String[] args) {
+        //Actividad: Secuencia fibonacci
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Cuántos números quieres de salida: ");
+        short valor = scanner.nextShort();
+        int a = 0, b = 1, c = 0;
+        System.out.println("======[Con FOR]======[");
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 1; i <= valor - 2; i++) {
+            c = a + b;
+            System.out.println(c);
+            a = b;
+            b = c;
+        }
+
+
+    }
 }
