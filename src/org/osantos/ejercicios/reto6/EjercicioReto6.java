842e50b0b85ad9670656760827170117b118de66
Oswaldo <santoswaldos@gmail.com>
2026-04-07 21:00:30 -0600

    Reto 6, secuencia fibonacci con FOR, WHILE y DO-WHILE

--- src/org/osantos/ejercicios/reto6/EjercicioReto6.java
+++ src/org/osantos/ejercicios/reto6/EjercicioReto6.java
@@ -9,16 +9,50 @@ public class EjercicioReto6 {
         System.out.print("Cuántos números quieres de salida: ");
         short valor = scanner.nextShort();
         int a = 0, b = 1, c = 0;
-        System.out.println("======[Con FOR]======[");
+        System.out.println("======[Con FOR]======");
         System.out.println(a);
         System.out.println(b);
-        for (int i = 1; i <= valor - 2; i++) {
+        for (int i = 1; i <= (valor - 2); i++) {
             c = a + b;
             System.out.println(c);
             a = b;
             b = c;
         }
 
+        System.out.println("======[Con WHILE]======");
+        a = 0;
+        b = 1;
+        c = 0;
+        int count = 1;
+        System.out.println(a);
+        System.out.println(b);
+        while (count <= (valor - 2)) {
+            c = a + b;
+            System.out.println(c);
+            a = b;
+            b = c;
+            count++;
+        }
+
+        System.out.println("======[Con DO-WHILE]======");
+        a = 0;
+        b = 1;
+        c = 0;
+        count = 1;
+        System.out.println(a);
+        System.out.println(b);
+        do {
+            if (valor <= 2) {
+                break;
+            }
+            c = a + b;
+            System.out.println(c);
+            a = b;
+            b = c;
+            count++;
+        } while (count <= (valor - 2));
+
+//        System.out.println("======[Con Recursividad]======"); //Esto es con un for y creando una función
 
     }
 }
